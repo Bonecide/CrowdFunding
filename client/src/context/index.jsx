137c8130ab9de0755fac282da1fbf1438da355d2
Kasymov Marsel <marselkasymov23@gmail.com>
2022-12-24 16:21:30 +0600

    фыв

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x3842Ea09606ca9B1C7Ea9AEf2F773ec66Ff57cD9');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
